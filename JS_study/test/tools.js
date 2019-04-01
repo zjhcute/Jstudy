f2abb32e6d2f89bd01a64028343754a03aa5e08e
zjhcute <1046596241@qq.com>
2019-04-01 21:46:40 +0800

    2019/4/1/21:46

--- JS_study/test/tools.js
+++ JS_study/test/tools.js
@@ -99,3 +99,32 @@ function cancelHandler(evnet) {
 }
 
 
+// 究极运动缓冲函数
+function startMove(oDom, attrObj, callBack) {
+    clearInterval(oDom.timer);
+    var iSpeed = null, iCur = null;
+    oDom.timer = setInterval(function() {
+        var bStop = true;
+        for(var attr in attrObj) {
+            if(attr == 'opacity') {
+                iCur = parseFloat(getStyle(oDom, attr) * 100);
+            }else{
+                iCur = parseInt(getStyle(oDom, attr));
+            }
+            iSpeed = (attrObj[attr] - iCur) / 7;
+            iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+            if(attr == 'opacity') {
+                oDom.style[attr] = (iCur + iSpeed) / 100;
+            }else {
+                oDom.style[attr] = iCur + iSpeed + 'px';
+            }
+            if(iCur != attrObj[attr]) {
+                bStop = false;
+            }
+        }
+        if(bStop) {
+            clearInterval(oDom.timer);
+            typeof callBack == 'function' && callBack();
+        }
+    }, 30)
+}
